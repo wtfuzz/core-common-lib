022c1617595205f1fa9f04dd8a71deaabcdb29e6
Zachary Crockett <towynlin@gmail.com>
2013-09-12 15:28:37 -0500

    Remove spark_wlan (to core-firmware)

--- build/SPARK_Firmware_Driver/src/subdir.mk
+++ build/SPARK_Firmware_Driver/src/subdir.mk
@@ -3,7 +3,6 @@
 C_SRCS += \
 ../SPARK_Firmware_Driver/src/cc3000_spi.c \
 ../SPARK_Firmware_Driver/src/hw_config.c \
-../SPARK_Firmware_Driver/src/spark_wlan.c \
 ../SPARK_Firmware_Driver/src/sst25vf_spi.c \
 ../SPARK_Firmware_Driver/src/system_stm32f10x.c \
 ../SPARK_Firmware_Driver/src/usb_pwr.c 
@@ -11,7 +10,6 @@ C_SRCS += \
 OBJS += \
 ./SPARK_Firmware_Driver/src/cc3000_spi.o \
 ./SPARK_Firmware_Driver/src/hw_config.o \
-./SPARK_Firmware_Driver/src/spark_wlan.o \
 ./SPARK_Firmware_Driver/src/sst25vf_spi.o \
 ./SPARK_Firmware_Driver/src/system_stm32f10x.o \
 ./SPARK_Firmware_Driver/src/usb_pwr.o 
@@ -19,7 +17,6 @@ OBJS += \
 C_DEPS += \
 ./SPARK_Firmware_Driver/src/cc3000_spi.d \
 ./SPARK_Firmware_Driver/src/hw_config.d \
-./SPARK_Firmware_Driver/src/spark_wlan.d \
 ./SPARK_Firmware_Driver/src/sst25vf_spi.d \
 ./SPARK_Firmware_Driver/src/system_stm32f10x.d \
 ./SPARK_Firmware_Driver/src/usb_pwr.d 
